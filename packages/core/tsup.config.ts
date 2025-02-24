f0181a872ddd061526f4219b8f464022202aa937
Sunny-117 <zhiqiangfu6@gmail.com>
2025-02-24 20:51:48 +0800

    chore: release v0.0.1-alpha.2

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,5 +1,9 @@
 import { createTsupConfig } from '../../scripts/tsup.base';
 
 export default createTsupConfig({
-  external: ['@ureq/lib-cache-store', '@ureq/lib-hash']
+  entry: ['src/index.ts'],
+  external: ['@ureq/lib-cache-store', '@ureq/lib-hash'],
+  noExternal: [],
+  clean: true,
+  outDir: 'dist'
 }); 
