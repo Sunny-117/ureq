ff77ad87320c4c2266ae2468e519aab04f0fdc1f
Sunny-117 <zhiqiangfu6@gmail.com>
2025-09-21 21:43:23 +0800

    feat: playground build by tsup

--- packages/playground/tsup.config.ts
+++ packages/playground/tsup.config.ts
@@ -1,7 +1,7 @@
 import { createTsupConfig } from '../../scripts/tsup.base';
 
 export default createTsupConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/demos/*.ts'],
   external: ['@ureq/core', '@ureq/impl-fetch', '@ureq/impl-axios'],
   noExternal: [],
   clean: true,
